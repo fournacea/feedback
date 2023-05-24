ea3a33b3b3e4ba007669ecaa7fba78d0dbe7b48d
Adam Fournace <fournacea@gmail.com>
2023-05-24 12:20:33 -0400

    ADD a nameform

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 
-def review(self):
-    return HttpResponse("Hello World!")
+def review(request):
+    if request.method == "POST":
+        entered_username = request.POST["username"]
+        current_csrf_token = request.POST["csrfmiddlewaretoken"]
+        print(f"USERNAME: {entered_username}")
+        print(f"CSRF TOKEN: {current_csrf_token}")
+        return HttpResponseRedirect("thank-you")
+    
+    return render(request, "reviews/review.html")
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
