5660fd85a70312c237716b1ed3e0ab7153ffdd3d
Adam Fournace <fournacea@gmail.com>
2023-05-25 07:40:34 -0400

    ADD review_text and rating fields to form.py

--- reviews/forms.py
+++ reviews/forms.py
@@ -6,3 +6,6 @@ class ReviewForm(forms.Form):
         "required": "Your name must not be empty",
         "max_length": "Name must be less than 100 characters",
     })
+
+    review_text = forms.CharField(label="Feedback:", widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(label="Rating:", min_value=1, max_value=5)
