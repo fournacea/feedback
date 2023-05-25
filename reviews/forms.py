134a3b1e2d60a2e538e71364eca7d8e1e054fc33
Adam Fournace <fournacea@gmail.com>
2023-05-25 17:11:09 -0400

    DELETE model for reviews, ADD modelform for reviews

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 
+from .models import Review
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your Name:", max_length=100, error_messages={
+#         "required": "Your name must not be empty",
+#         "max_length": "Name must be less than 100 characters",
+#     })
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Your Name:", max_length=100, error_messages={
-        "required": "Your name must not be empty",
-        "max_length": "Name must be less than 100 characters",
-    })
+#     review_text = forms.CharField(label="Feedback:", widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(label="Rating:", min_value=1, max_value=5)
 
-    review_text = forms.CharField(label="Feedback:", widget=forms.Textarea, max_length=200)
-    rating = forms.IntegerField(label="Rating:", min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
