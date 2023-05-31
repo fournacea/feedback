531ce51c407131191edb55698bfbd2b1f363443a
Adam Fournace <fournacea@gmail.com>
2023-05-30 21:20:54 -0400

    CHANGE Review view to class based view

--- reviews/views.py
+++ reviews/views.py
@@ -1,31 +1,57 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
 
 from .forms import ReviewForm
 
-from .models import Review
+# from .models import Review
 
 # Create your views here.
 
-def review(request):
-    ##-- To update instead of create new review --##
-    # if request.method == "POST":
-    # existing_data = Review.objects.get(pk=4)
-    # form = ReviewForm(request.POST, instance=existing_data)
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
 
-    if request.method == "POST":
+        return render(request, "reviews/review.html",{
+            "form": form
+        })
+
+
+    def post(self, request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("thank-you")
         
-    else:
-        form = ReviewForm()
+        else:
+            return render(request, "reviews/review.html",{
+                "form": form
+            })
 
-    return render(request, "reviews/review.html",{
-        "form": form
-    })
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
+
+
+
+# def review(request):
+#     ##-- To update instead of create new review --##
+#     # if request.method == "POST":
+#     # existing_data = Review.objects.get(pk=4)
+#     # form = ReviewForm(request.POST, instance=existing_data)
+
+#     if request.method == "POST":
+#         form = ReviewForm(request.POST)
+
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("thank-you")
+        
+#     else:
+#         form = ReviewForm()
+
+#     return render(request, "reviews/review.html",{
+#         "form": form
+#     })
+
