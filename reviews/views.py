ee18c543cc1a306c5e49c4ccbfb6c23d6f9c1b3d
Adam Fournace <fournacea@gmail.com>
2023-06-01 10:25:18 -0400

    ADD SingleReviewView

--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,12 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 
-# from .models import Review
+from .models import Review
 
 # Create your views here.
 
@@ -30,8 +32,33 @@ class ReviewView(View):
             })
 
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+    
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        selected_review = Review.objects.get(pk=review_id) #Need try/except
+        context["review"] = selected_review
+        return context
+                                # except block: return 404 page
+
+    
+
+
+
+
+# def thank_you(request):
+#     return render(request, "reviews/thank_you.html")
 
 
 
