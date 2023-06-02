d2e7b8ed381e8e95f2486cd6fa01f7ae664d9787
Adam Fournace <fournacea@gmail.com>
2023-06-02 13:56:52 -0400

    ADD profiles app with basic css

--- reviews_app/settings.py
+++ reviews_app/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # My Apps
-    'reviews'
+    'reviews',
+    'profiles',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    'profiles/static/profiles/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
