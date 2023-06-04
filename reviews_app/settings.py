955069a4ab99e7adcd72537c0cbd3bf30b3bbce8
Adam Fournace <fournacea@gmail.com>
2023-06-04 13:32:05 -0400

    CHANGE ReviewView to use CreateView, file upload fully functional, ADD session capability

--- reviews_app/settings.py
+++ reviews_app/settings.py
@@ -127,6 +127,9 @@ STATIC_URL = 'static/'
 # ]
 
 MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/user-media/'
+
+SESSION_COOKIE_AGE =  3600
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
